1190ff1d7ef06c91833997d7a6c5df9228669e83
Dominic Charles <dominic@macbook-pro.home>
2020-06-13 14:53:04 +0100

    filter tasks

--- app.js
+++ app.js
@@ -11,28 +11,56 @@ loadEvenListeners();
 // Add Task
 function loadEvenListeners() {
   form.addEventListener("submit", addTask);
+  taskList.addEventListener("click", removeTask);
+  clearbtn.addEventListener("click", clearTasks);
+  filter.addEventListener("keyup", filterTasks);
 }
 
 function addTask(e) {
-  if (taskInput.value === " ") {
+  if (taskInput.value === "") {
     alert("Please enter a task");
+  } else {
+    // Create li tag
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    // Create a tag
+    const link = document.createElement("a");
+    link.className = "delete-item secondary-content";
+
+    // Insert I (icon) to
+    link.innerHTML = "<i class= 'fa fa-remove'></i>";
+
+    li.appendChild(link);
+    taskList.appendChild(li);
+
+    taskInput.value = "";
   }
 
-  // Create li
-  const li = document.createElement("li");
-  li.className = "collection-item";
-  li.appendChild(document.createTextNode(taskInput.value));
-
-  // Create a
-  const link = document.createElement("a");
-  link.className = "delete-item secondary-content";
-  link.innerHTML = "<i class= 'fa fa-remove'></i>";
-
-  li.appendChild(link);
-
-  taskList.appendChild(li);
-
-  taskInput.value = "";
-
   e.preventDefault();
+}
+
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearTasks(e) {
+  taskList.innerHTML = "";
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  const lis = document.querySelectorAll(".collection-item");
+
+  lis.forEach(function (li) {
+    const item = li.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) > -1) {
+      li.style.display = "block";
+    } else {
+      li.style.display = "none";
+    }
+  });
 }
